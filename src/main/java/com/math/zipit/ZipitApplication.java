85cb1f038fd72a62aa1fa06b3d9aea5188113750
Matheesh <matheeshprabhu123@gmail.com>
2023-09-08 12:02:05 +0530

    initial commit

---
+++ src/main/java/com/math/zipit/ZipitApplication.java
@@ -0,0 +1,13 @@
+package com.math.zipit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math/zipit/ZipitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math.zipit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
